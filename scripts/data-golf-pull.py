f7c68fb4926c49f3102888361a45206d0ac6916d
highlyprofitable108 <mikefusc@me.com>
2023-05-23 21:21:57 -0400

    Update data-golf-pull.py

--- scripts/data-golf-pull.py
+++ scripts/data-golf-pull.py
@@ -36,65 +36,50 @@ file_format = 'csv'
 # Set event_id as 'all'
 event_id = 'all'
 
+
+def handle_error(error_message):
+    print(f"An error occurred: {error_message}")
+    # Additional error handling logic or notifications can be added here
+
+
+def get_data_from_api(endpoint):
+    try:
+        response = requests.get(endpoint)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.HTTPError as err:
+        handle_error(f"HTTP error occurred: {err}")
+    except requests.exceptions.RequestException as err:
+        handle_error(f"An exception occurred: {err}")
+    except ValueError as err:
+        handle_error(f"Error parsing JSON: {err}")
+    return None
+
+
 def get_historical_raw_data_event_ids(file_format='json'):
     endpoint = f"{base_url}/historical-raw-data/event-list?file_format={file_format}&key={api_key}"
-    response = requests.get(endpoint)
-    try:
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-        print(f"HTTP error occurred: {err}")
-    except json.JSONDecodeError:
-        logging.warning(f"Bad data received from API for endpoint: {endpoint}")
-    else:
-        return response.json()
+    return get_data_from_api(endpoint)
+
 
 def get_round_scoring_stats_strokes_gained(tour, event_id, year, file_format='json'):
     endpoint = f"{base_url}/historical-raw-data/rounds?tour={tour}&event_id={event_id}&year={year}&file_format={file_format}&key={api_key}"
-    response = requests.get(endpoint)
-    try:
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-        print(f"HTTP error occurred: {err}")
-    except json.JSONDecodeError:
-        logging.warning(f"Bad data received from API for endpoint: {endpoint}")
-    else:
-        return response.json()
+    return get_data_from_api(endpoint)
+
 
 def get_historical_odds_data_event_ids(tour='pga', file_format='json'):
     endpoint = f"{base_url}/historical-odds/event-list?tour={tour}&file_format={file_format}&key={api_key}"
-    response = requests.get(endpoint)
-    try:
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-        print(f"HTTP error occurred: {err}")
-    except json.JSONDecodeError:
-        logging.warning(f"Bad data received from API for endpoint: {endpoint}")
-    else:
-        return response.json()
+    return get_data_from_api(endpoint)
+
 
 def get_historical_outrights(tour, event_id, year, market, book, odds_format='decimal', file_format='json'):
     endpoint = f"{base_url}/historical-odds/outrights?tour={tour}&event_id={event_id}&year={year}&market={market}&book={book}&odds_format={odds_format}&file_format={file_format}&key={api_key}"
-    response = requests.get(endpoint)
-    try:
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-        print(f"HTTP error occurred: {err}")
-    except json.JSONDecodeError:
-        logging.warning(f"Bad data received from API for endpoint: {endpoint}")
-    else:
-        return response.json()
+    return get_data_from_api(endpoint)
+
 
 def get_historical_matchups(tour, event_id, year, book, odds_format='decimal', file_format='json'):
     endpoint = f"{base_url}/historical-odds/matchups?tour={tour}&event_id={event_id}&year={year}&book={book}&odds_format={odds_format}&file_format={file_format}&key={api_key}"
-    response = requests.get(endpoint)
-    try:
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-        print(f"HTTP error occurred: {err}")
-    except json.JSONDecodeError:
-        logging.warning(f"Bad data received from API for endpoint: {endpoint}")
-    else:
-        return response.json()
+    return get_data_from_api(endpoint)
+
 
 # Loop through all tours and years
 for tour in tours:
@@ -106,12 +91,17 @@ for tour in tours:
             # Print debugging information
             print(f"Fetching data for tour: {tour}, event_id: {event_id}, year: {year}, book: {book}")
 
-            # Call API
+            # Call API and retrieve data
             raw_data = get_round_scoring_stats_strokes_gained(tour, event_id, year, file_format)
             outrights = get_historical_outrights(tour, event_id, year, market, book, odds_format, file_format)
             matchups = get_historical_matchups(tour, event_id, year, book, odds_format, file_format)
 
-            # Convert to DataFrame
+            # Check if data retrieval was successful
+            if raw_data is None or outrights is None or matchups is None:
+                # Skip this iteration if data retrieval failed
+                continue
+
+            # Convert to DataFrames
             df_raw_data = pd.DataFrame(raw_data)
             df_outrights = pd.DataFrame(outrights)
             df_matchups = pd.DataFrame(matchups)
@@ -121,7 +111,9 @@ for tour in tours:
             df_total = pd.concat([df_total, df_temp], axis=0)
 
         # Save to CSV
-        df_total.to_csv(f'{csv_file_path}/historical_data_{tour}_{year}.csv', index=False)
+        file_name = f'historical_data_{tour}_{year}.csv'
+        file_path = os.path.join(csv_file_path, file_name)
+        df_total.to_csv(file_path, index=False)
 
         # Print some data for debugging
         print(df_total.head())
