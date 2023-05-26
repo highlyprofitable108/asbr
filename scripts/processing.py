6450d971879d5022f088511010d86e1f4c30ee0c
highlyprofitable108 <mikefusc@me.com>
2023-05-26 19:52:02 -0400

    code sync

--- scripts/processing.py
+++ scripts/processing.py
@@ -1,68 +1,221 @@
-import pandas as pd
-import numpy as np
 import sqlite3
-from sklearn.model_selection import train_test_split, GridSearchCV
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.feature_selection import SelectKBest, chi2
 from sklearn.linear_model import Ridge
-from sklearn.pipeline import Pipeline
-from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
-from sklearn.preprocessing import StandardScaler
 
-# Connect to SQLite database
-database_path = '/Users/michaelfuscoletti/Desktop/data/pgatour.db'
-conn = sqlite3.connect(database_path)
-cursor = conn.cursor()
+# Database connection function
+def connect_to_database(database_name):
+    try:
+        conn = sqlite3.connect(database_name)
+        print("Database connection successful.")  # Debugging line
+        return conn
+    except Exception as e:
+        print(f"Database connection error: {e}")  # Debugging line
 
-def train_model(data, target_column):
-    # Separate target variable from the rest of the data
-    y = data[target_column]
-    X = data.drop(target_column, axis=1)
+# Data retrieval function
+def retrieve_data(connection, table_name):
+    try:
+        query = f"SELECT * FROM {table_name}"
+        cursor = connection.cursor()
+        cursor.execute(query)
+        data = cursor.fetchall()
+        columns = [desc[0] for desc in cursor.description]
+        df = pd.DataFrame(data, columns=columns)
+        print(f"Data retrieval from professional tours successful.")  # Debugging line
+        return df
+    except Exception as e:
+        print(f"Data retrieval error: {e}")  # Debugging line
 
-    # Add 'round_score_std' as a feature
-    X = X.join(data['round_score_std'])
+# Data exploration and preprocessing function
+def explore_and_preprocess_data(df):
+    # Create a copy of the DataFrame to avoid SettingWithCopyWarning
+    df_copy = df.copy()
 
-    # Split the data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    # Check and filter round_score column
+    df_copy.loc[:, 'round_score'] = pd.to_numeric(df_copy['round_score'], errors='coerce').fillna(-999)
+    df_copy = df_copy[(df_copy['round_score'] >= 50) & (df_copy['round_score'] <= 90) | (df_copy['round_score'] == -999)]
 
-    # Create a pipeline with StandardScaler and Ridge regression
-    model = Pipeline([
-        ('scaler', StandardScaler()),
-        ('ridge', Ridge())
-    ])
+    # Check and filter sg columns
+    sg_columns = ['sg_putt', 'sg_arg', 'sg_app', 'sg_ott', 'sg_t2g', 'sg_total']
+    for column in sg_columns:
+        df_copy.loc[:, column] = pd.to_numeric(df_copy[column], errors='coerce').fillna(-999).round(3)
+        df_copy = df_copy[(df_copy[column] >= -10) & (df_copy[column] <= 10) | (df_copy[column] == -999)]
 
-    # Define the hyperparameters and their possible values for fine-tuning
-    params = {
-        'ridge__alpha': [1e-3, 1e-2, 1e-1, 1, 10, 100]
-    }
+    # Check round_num column
+    df_copy.loc[:, 'round_num'] = pd.to_numeric(df_copy['round_num'], errors='coerce').fillna(-999)
+    df_copy = df_copy[(df_copy['round_num'] >= 1) & (df_copy['round_num'] <= 4) | (df_copy['round_num'] == -999)]
 
-    # Perform a grid search with cross-validation to find the best hyperparameters
-    grid_search = GridSearchCV(model, params, cv=5, scoring='neg_mean_absolute_error', n_jobs=-1)
-    grid_search.fit(X_train, y_train)
+    # Check course_par column
+    df_copy.loc[:, 'course_par'] = pd.to_numeric(df_copy['course_par'], errors='coerce').fillna(-999)
+    df_copy = df_copy[(df_copy['course_par'] >= 69) & (df_copy['course_par'] <= 75) | (df_copy['course_par'] == -999)]
 
-    # Train the model with the best hyperparameters on the entire training set
-    best_model = grid_search.best_estimator_
-    best_model.fit(X_train, y_train)
+    # Check percentage columns
+    percentage_columns = ['gir', 'driving_acc', 'scrambling']
+    for column in percentage_columns:
+        df_copy.loc[:, column] = pd.to_numeric(df_copy[column], errors='coerce').fillna(-999)
+        df_copy = df_copy[(df_copy[column] >= 0) & (df_copy[column] <= 100) | (df_copy[column] == -999)]
 
-    # Evaluate the model on the test set
-    y_pred = best_model.predict(X_test)
-    mae = mean_absolute_error(y_test, y_pred)
-    print(f'Mean Absolute Error: {mae:.2f}')
+    # Normalize sg data relative to field strength
+    sg_columns_relative = ['sg_putt', 'sg_arg', 'sg_app', 'sg_ott', 'sg_t2g', 'sg_total']
+    for column in sg_columns_relative:
+        df_copy[column] = pd.to_numeric(df_copy[column], errors='coerce')
+        df_copy[column].fillna(-999, inplace=True)
+        df_copy[column] = df_copy.groupby('event_id')[column].transform(lambda x: MinMaxScaler().fit_transform(x.values.reshape(-1, 1)).flatten())
 
-    return best_model
+    # Selecting features based on chi-square test for round scores
+    round_scores = df_copy[['round_score']]
+    round_scores = round_scores.astype(int)  # Ensure round scores are integer values
+    round_scores = round_scores.apply(lambda x: pd.qcut(x, q=4, labels=False, duplicates='drop'))
+    round_scores = round_scores.astype(str)  # Convert back to string type for chi-square test
+    round_scores = round_scores.apply(lambda x: x.replace('NaN', 'Missing'))  # Handle missing values
 
-def main():
-    # Load data from the database
-    query = "SELECT * FROM raw_data"
-    data = pd.read_sql_query(query, conn)
+    chi2_selector = SelectKBest(chi2, k='all')
+
+    # Remove leading 'T' from the 'fin_text' column
+    df_copy['fin_text'] = df_copy['fin_text'].str.lstrip('T')
+
+    # Replace non-numeric values with NaN and then with '999'
+    df_copy['fin_text'] = pd.to_numeric(df_copy['fin_text'], errors='coerce').fillna(999)
+
+    # Convert 'fin_text' column to int
+    df_copy['fin_text'] = df_copy['fin_text'].astype(int)
+
+    round_scores_selected = chi2_selector.fit_transform(round_scores, df_copy['fin_text'])
+    round_scores_selected = pd.DataFrame(round_scores_selected, columns=['round_score'])
+
+    # Normalize tour and course_name columns if needed
+    df_copy['tour'] = df_copy['tour'].str.strip()  # Remove leading/trailing spaces
+
+    # Applying ridge regression for feature selection on golf course information
+    course_info = df_copy[['course_name', 'course_par', 'course_num']]
+    course_info_encoded = pd.get_dummies(course_info, drop_first=True)
+    course_info_encoded_normalized = StandardScaler().fit_transform(course_info_encoded)
+
+    ridge_selector = Ridge(alpha=1.0)
+    ridge_selector.fit(course_info_encoded_normalized, df_copy['fin_text'])
+
+    # Concatenating selected features back to the original dataframe
+    df_selected = pd.concat([df_copy[['fin_text']], round_scores_selected, course_info_encoded], axis=1)
     
-    # Preprocess the data
-    data = preprocess_data(data)
+    # Step 3.5: Dimensionality Reduction
+    df_preprocessed = dimensionality_reduction(df_selected)
+
+    # Step 3.4: Encoding Categorical Variables
+    # categorical_columns = ['tour', 'year', 'season', 'event_name', 'round_num']
+
+    # One-Hot Encoding with column dropping
+    # df_encoded = pd.get_dummies(df_selected, columns=categorical_columns, drop_first=True)
+
+    # Handling missing values in categorical variables
+    # df_encoded.fillna('Missing', inplace=True)
+
+    # Concatenating the encoded categorical variables back to the dataframe
+    # df_preprocessed = pd.concat([df_encoded, df_selected.drop(categorical_columns, axis=1)], axis=1)
+
+    # Step 3.6: Data Sampling
+    # Placeholder for data sampling techniques
+    # Add your code here for handling imbalanced datasets if needed
+
+    # Step 3.7: Handling Skewed Data
+    # Placeholder for handling skewed data
+    # Add your code here for handling skewed features
+
+    # Step 3.8: Outlier Detection and Treatment
+    # Placeholder for outlier detection and treatment
+    # Add your code here for outlier detection and treatment techniques
+
+    # Step 3.9: Optimal Simulation
+    # Placeholder for optimal simulation
+    # Add your code here for optimal simulation techniques
+
+    # Step 3.10: Mutual Information Feature Selection
+    # Placeholder for mutual information feature selection
+    # Add your code here for mutual information feature selection techniques
+
+    # Placeholder for additional preprocessing steps or enhancements
+    # Add your code or placeholders for any other important preprocessing steps
+
+    print("Data exploration and preprocessing complete.")  # Debugging line
+
+    return df_preprocessed
+
+def dimensionality_reduction(df):
+    print("Starting dimensionality reduction.")  # Debugging line
     
-    # Train the model
-    target_column = 'round_score_sum'
-    model = train_model(data, target_column)
+    # Select the relevant features for PCA
+    features = df.drop(['fin_text'], axis=1)
 
-    # Close the database connection
-    conn.close()
+    # Replace missing or infinite values with a large negative number
+    features = features.replace([np.inf, -np.inf], np.nan)
+    features.fillna(-999, inplace=True)
 
-if __name__ == "__main__":
-    main()
+    # Perform PCA
+    pca = PCA(n_components=7)  # Adjust the number of components as needed
+    features_reduced = pca.fit_transform(features)
+    
+    # Create a DataFrame with the reduced features
+    df_reduced = pd.DataFrame(features_reduced, columns=[f'pc_{i+1}' for i in range(pca.n_components_)])
+    
+    # Concatenate the reduced features with the target variable
+    df_processed = pd.concat([df[['fin_text']], df_reduced], axis=1)
+    
+    print(f"Explained Variance Ratio: {pca.explained_variance_ratio_}")
+    cumulative_variance = np.cumsum(pca.explained_variance_ratio_)
+    print(f"Cumulative Explained Variance: {cumulative_variance}")
+
+    plt.plot(pca.explained_variance_ratio_)
+    plt.xlabel('Number of Components')
+    plt.ylabel('Variance (%)') # for each component
+    plt.title('Explained Variance')
+    plt.show()
+
+    print(df_processed.head())
+
+    print("Dimensionality reduction complete.")  # Debugging line
+    return df_processed
+
+# Feature engineering function
+def feature_engineering(df):
+    print("Starting prediction engine.")  # Debugging line
+    # Add your feature engineering steps here
+    print("Prediction engine running.")  # Debugging line
+    return df
+
+# Algorithm development function
+def algorithm_development(df):
+    print("Starting algorithm development.")  # Debugging line
+    # Add your algorithm development steps here
+    print("Algorithm development complete.")  # Debugging line
+    # return model
+
+# Prediction function
+def predict_future_tournaments(df, model):
+    print("Starting sim of upcoming tournament.")  # Debugging line
+    # Add your prediction steps here
+    print("Prediction of future tournaments complete.")  # Debugging line
+    # return predictions
+
+# Evaluation function
+def analyze_and_evaluate_predictions(predictions, actual_results):
+    print("Starting analysis of best bets.")  # Debugging line
+    # Add your analysis and evaluation steps here
+    print("Almost there!")  # Debugging line
+    # return evaluation_metrics
+
+# Main execution flow
+if __name__ == '__main__':
+    try:
+        conn = connect_to_database('/Users/michaelfuscoletti/Desktop/data/pgatour.db')
+        data = retrieve_data(conn, 'raw_data')
+        data = explore_and_preprocess_data(data)
+        data = feature_engineering(data)
+        model = algorithm_development(data)
+        # predictions = predict_future_tournaments(data, model)
+        # evaluation_metrics = analyze_and_evaluate_predictions(predictions, actual_results)
+        # print("Execution complete.")  # Debugging line
+    except Exception as e:
+        print(f"Execution error: {e}")  # Debugging line
