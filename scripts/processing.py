4f162d4a84267a3c38fc5f529cb3e04c80370157
highlyprofitable108 <mikefusc@me.com>
2023-05-25 22:19:18 -0400

    Update processing.py

--- scripts/processing.py
+++ scripts/processing.py
@@ -6,63 +6,178 @@ from sklearn.linear_model import Ridge
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
-# Connect to SQLite database
-database_path = '/Users/michaelfuscoletti/Desktop/data/pgatour.db'
-conn = sqlite3.connect(database_path)
-cursor = conn.cursor()
+# Step 1: Connect to SQLite Database
+def connect_to_database(database_name):
+    conn = sqlite3.connect(database_name)
+    return conn
 
-def train_model(data, target_column):
-    # Separate target variable from the rest of the data
-    y = data[target_column]
-    X = data.drop(target_column, axis=1)
+# Step 2: Retrieve Data
+def retrieve_data(connection, table_name):
+    query = f"SELECT * FROM {table_name}"
+    cursor = connection.cursor()
+    cursor.execute(query)
+    data = cursor.fetchall()
+    columns = [desc[0] for desc in cursor.description]
+    df = pd.DataFrame(data, columns=columns)
+    return df
 
-    # Add 'round_score_std' as a feature
-    X = X.join(data['round_score_std'])
+# Step 3: Data Exploration and Preprocessing
+def explore_and_preprocess_data(df):
+    # Check and filter round_score column
+    df = df[(df['round_score'] >= 50) & (df['round_score'] <= 90)]
 
-    # Split the data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    # Check and filter sg columns
+    sg_columns = ['sg_putt', 'sg_arg', 'sg_app', 'sg_ott', 'sg_t2g', 'sg_total']
+    for column in sg_columns:
+        df = df[(df[column] >= -10) & (df[column] <= 10)]
 
-    # Create a pipeline with StandardScaler and Ridge regression
-    model = Pipeline([
-        ('scaler', StandardScaler()),
-        ('ridge', Ridge())
-    ])
+    # Check round_num column
+    df = df[(df['round_num'] >= 1) & (df['round_num'] <= 4)]
 
-    # Define the hyperparameters and their possible values for fine-tuning
-    params = {
-        'ridge__alpha': [1e-3, 1e-2, 1e-1, 1, 10, 100]
-    }
+    # Check course_par column
+    df = df[(df['course_par'] >= 69) & (df['course_par'] <= 75)] 
 
-    # Perform a grid search with cross-validation to find the best hyperparameters
-    grid_search = GridSearchCV(model, params, cv=5, scoring='neg_mean_absolute_error', n_jobs=-1)
-    grid_search.fit(X_train, y_train)
+    # Check percentage columns
+    percentage_columns = ['gir', 'driving_acc', 'scrambling']
+    for column in percentage_columns:
+        df = df[(df[column] >= 0) & (df[column] <= 100)]
 
-    # Train the model with the best hyperparameters on the entire training set
-    best_model = grid_search.best_estimator_
-    best_model.fit(X_train, y_train)
+    # Convert date/time columns to appropriate data type
+    date_columns = ['open_time', 'close_time', 'event_completed']
+    for column in date_columns:
+        df[column] = pd.to_datetime(df[column])
 
-    # Evaluate the model on the test set
-    y_pred = best_model.predict(X_test)
-    mae = mean_absolute_error(y_test, y_pred)
-    print(f'Mean Absolute Error: {mae:.2f}')
+    # Normalize sg data relative to field strength
+    sg_columns_relative = ['sg_putt', 'sg_arg', 'sg_app', 'sg_ott', 'sg_t2g', 'sg_total']
+    for column in sg_columns_relative:
+        df[column] = df.groupby('event_id')[column].transform(lambda x: MinMaxScaler().fit_transform(x.values.reshape(-1, 1)))
 
-    return best_model
+    # Selecting features based on chi-square test for round scores
+    round_scores = df[['round_score']]
+    round_scores = round_scores.astype(int)  # Ensure round scores are integer values
+    round_scores = round_scores.apply(lambda x: pd.qcut(x, q=4, labels=False, duplicates='drop'))
+    round_scores = round_scores.astype(str)  # Convert back to string type for chi-square test
+    round_scores = round_scores.apply(lambda x: x.replace('NaN', 'Missing'))  # Handle missing values
 
-def main():
-    # Load data from the database
-    query = "SELECT * FROM raw_data"
-    data = pd.read_sql_query(query, conn)
+    chi2_selector = SelectKBest(chi2, k='all')
+    round_scores_selected = chi2_selector.fit_transform(round_scores, df['leaderboard_position'])
+    round_scores_selected = pd.DataFrame(round_scores_selected, columns=['round_score_selected'])
+
+    # Normalize tour and course_name columns if needed
+    df['tour'] = df['tour'].str.strip()  # Remove leading/trailing spaces
+
+    # Applying ridge regression for feature selection on golf course information
+    course_info = df[['course_name', 'course_par', 'course_num']]
+    course_info_encoded = pd.get_dummies(course_info, drop_first=True)
+    course_info_encoded_normalized = StandardScaler().fit_transform(course_info_encoded)
+
+    ridge_selector = Ridge(alpha=1.0)
+    ridge_selector.fit(course_info_encoded_normalized, df['leaderboard_position'])
+
+    # Concatenating selected features back to the original dataframe
+    df_selected = pd.concat([df[['leaderboard_position']], round_scores_selected, course_info_encoded], axis=1)
+
+    # Step 3.4: Encoding Categorical Variables
+    categorical_columns = ['tour', 'year', 'season', 'event_name', 'round_number']
+
+    # One-Hot Encoding with column dropping
+    df_encoded = pd.get_dummies(df_selected, columns=categorical_columns, drop_first=True)
+
+    # Handling missing values in categorical variables
+    df_encoded.fillna('Missing', inplace=True)
+
+    # Concatenating the encoded categorical variables back to the dataframe
+    df_preprocessed = pd.concat([df_encoded, df_selected.drop(categorical_columns, axis=1)], axis=1)
+
+    # Step 3.5: Dimensionality Reduction
+    df_preprocessed = dimensionality_reduction(df_preprocessed)
+
+    # Step 3.6: Data Sampling
+    # Placeholder for data sampling techniques
+    # Add your code here for handling imbalanced datasets if needed
+
+    # Step 3.7: Handling Skewed Data
+    # Placeholder for handling skewed data
+    # Add your code here for handling skewed features
+
+    # Step 3.8: Outlier Detection and Treatment
+    # Placeholder for outlier detection and treatment
+    # Add your code here for outlier detection and treatment techniques
+
+    # Step 3.9: Optimal Simulation
+    # Placeholder for optimal simulation
+    # Add your code here for optimal simulation techniques
+
+    # Step 3.10: Mutual Information Feature Selection
+    # Placeholder for mutual information feature selection
+    # Add your code here for mutual information feature selection techniques
+
+    # Placeholder for additional preprocessing steps or enhancements
+    # Add your code or placeholders for any other important preprocessing steps
+
+    return df_preprocessed
+
+# Step 3.5: Dimensionality Reduction
+def dimensionality_reduction(df):
+    # Select the relevant features for PCA
+    features = df.drop(['leaderboard_position'], axis=1)
     
-    # Preprocess the data
-    data = preprocess_data(data)
+    # Perform PCA
+    pca = PCA(n_components=7)  # Adjust the number of components as needed
+    features_reduced = pca.fit_transform(features)
     
-    # Train the model
-    target_column = 'round_score_sum'
-    model = train_model(data, target_column)
+    # Create a DataFrame with the reduced features
+    df_reduced = pd.DataFrame(features_reduced, columns=[f'pc_{i+1}' for i in range(pca.n_components_)])
+    
+    # Concatenate the reduced features with the target variable
+    df_processed = pd.concat([df[['leaderboard_position']], df_reduced], axis=1)
+    
+    return df_processed
 
-    # Close the database connection
-    conn.close()
+# Step 4: Feature Engineering
+def feature_engineering(df):
+    # Placeholder for feature engineering steps
+    # Add your code here for creating new features or transforming existing ones
 
-if __name__ == "__main__":
-    main()
+    return df
+
+# Step 5: Algorithm Development
+def algorithm_development(df):
+    # Placeholder for algorithm development
+    # Add your code here to train and evaluate the algorithm
+
+    return model
+
+# Step 6: Predicting Future Tournaments
+def predict_future_tournaments(df, model):
+    # Placeholder for predicting future tournaments
+    # Add your code here to preprocess upcoming tournament data and use the trained model for predictions
+
+    return predictions
+
+# Step 7: Analysis and Evaluation
+def analyze_and_evaluate_predictions(predictions, actual_results):
+    # Placeholder for analysis and evaluation
+    # Add your code here to compare the predictions with actual results, assess the performance, and refine the algorithm as needed
+
+    return evaluation_metrics
+
+# Step 8: Deployment and Integration
+def deploy_and_integrate():
+    # Placeholder for deployment and integration
+    # Add your code here to integrate the algorithm into your system or application for real-time or automated predictions
+
+    return
+
+if __name__ == '__main__':
+    # Main execution flow
+    conn = connect_to_database('your_database.db')
+    data = retrieve_data(conn, 'your_table')
+    data = explore_and_preprocess_data(data)
+    data = feature_engineering(data)
+    model = algorithm_development(data)
+    predictions = predict_future_tournaments(data, model)
+    evaluation_metrics = analyze_and_evaluate_predictions(predictions, actual_results)
+    deploy_and_integrate()
